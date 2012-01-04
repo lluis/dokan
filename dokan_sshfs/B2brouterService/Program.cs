4790b38cf7303e9b6a2b2c62eecdb7e12d554c37
Lluis Gili <lluis@ingent.net>
2012-01-04 10:31:26 +0100

    self install service

--- dokan_sshfs/B2brouterService/Program.cs
+++ dokan_sshfs/B2brouterService/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Configuration.Install;
+using System.Reflection;
 
 namespace DokanSSHFS
 {
@@ -16,14 +18,32 @@ namespace DokanSSHFS
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-			{ 
-				new B2brouterService() 
-			};
-            ServiceBase.Run(ServicesToRun);
+            if (System.Environment.UserInteractive)
+            {
+                string parameter = string.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        break;
+                }
+            }
+            else
+            {
+                ServiceBase.Run(new B2brouterService());
+            }
+
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[] 
+			//{ 
+			//	new B2brouterService() 
+			//};
+            //ServiceBase.Run(ServicesToRun);
         }
     }
 }
